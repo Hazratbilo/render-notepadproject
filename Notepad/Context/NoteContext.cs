00a3d755ca09d18c3ac154acf84fffc7ec14a6da
Hazratbilo <alatoyebilal14@gmail.com>
2025-12-03 17:37:54 +0100

    context settings update

--- Notepad/Context/NoteContext.cs
+++ Notepad/Context/NoteContext.cs
@@ -15,7 +15,7 @@ namespace Notepad.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseNpgsql(" \"Host=dpg-d4necv7gi27c738hs540-a.oregon-postgres.render.com;Port=5432;Database=notepad_axz5;Username=notepad_axz5_user;Password=gdzZ0BUZIwUk9p9a0NBQuCbrAUB5hNDM;SSL Mode=Require;Trust Server Certificate=true\";")
+                .UseNpgsql("Host=dpg-d4necv7gi27c738hs540-a.oregon-postgres.render.com;Port=5432;Database=notepad_axz5;Username=notepad_axz5_user;Password=gdzZ0BUZIwUk9p9a0NBQuCbrAUB5hNDM;SSL Mode=Require;Trust Server Certificate=true;")
                 .ConfigureWarnings(w =>
                     w.Ignore(RelationalEventId.PendingModelChangesWarning));
         }
